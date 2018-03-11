c7fb14acd38d1b1be5029465153cc29daf324f5f
Mikołaj Obliński <mikolaj.oblinski@gmail.com>
2018-03-11 14:02:40 +0100

    obiekt Math omówienie

---
+++ warsztaty/java-script/js/math.js
@@ -0,0 +1,9 @@
+//wlasciwosci obiektu Math
+console.log( Math.E);
+console.log( Math.PI );
+console.log( Math.SQRT2);
+
+//metody obiektu math
+console.log( Math.cos(30));
+
+// wszystkie metody sa na stronie https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Math
