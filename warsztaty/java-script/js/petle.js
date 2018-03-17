7b3a0cdf57bbe710b021d66e1c3c42f200e291ce
Mikołaj Obliński <mikolaj.oblinski@gmail.com>
2018-03-17 22:06:35 +0100

    dokumenty bez zmian

--- warsztaty/java-script/js/petle.js
+++ warsztaty/java-script/js/petle.js
@@ -1,10 +1,11 @@
+// petli najszesciej uzywa sie do wyswietlania danych z baz danych
 //pętla for
+
+for( var i = 1; i <= 10; i++ ) {
+	console.log("Iteracja nr");
+}
 //
-//for( var i = 1; i <= 10; i++ ) {
-//	console.log("Iteracja nr");
-//}
-//
-var tablica = ["volvo", "Mazda", "Honda", "Audi"];
+//var tablica = ["volvo", "Mazda", "Honda", "Audi"];
 //for( var i= 0; i < tablica.length; i++) {
 //	console.log(tablica[i]);
 //}
@@ -24,23 +25,23 @@ var tablica = ["volvo", "Mazda", "Honda", "Audi"];
 //var iter = 1;
 //do {
 //	console.log("while iteracja nr " + iter);
-//	tu mam nie dokończony kod
-//} while();
+//	iter++;
+//} while ( iter <= 25 );
 
 
 //brake i continue
-for( var i = 1; i < 15; i++ ) {
-if( i > 6 ) {
-	break;
-} else {
-	console.log("obrót petli nr " + i);
-}
-}
-
-for(var i=1; i<15; i++) {
-	if( i == 4 || i == 6 || i == 10) {
-		continue;
-	} else {
-		console.log( "iteracja nr " + i );
-	}
-}
+//for( var i = 1; i < 15; i++ ) {
+//if( i > 6 ) {
+//	break;
+//} else {
+//	console.log("obrót petli nr " + i);
+//}
+//}
+//
+//for(var i=1; i<15; i++) {
+//	if( i == 4 || i == 6 || i == 10) {
+//		continue;
+//	} else {
+//		console.log( "iteracja nr " + i );
+//	}
+//}
--- warsztaty/java-script/js/zasieg-zmiennych.js
+++ warsztaty/java-script/js/zasieg-zmiennych.js
@@ -1,14 +1,14 @@
-//var zmiennaGlobalna = "Tak jestem zmienną globalną i jestem widoczna w funkcjach";
-//function wyswietlZmienna() {
-//console.log(zmiennaGlobalna);
-//    var zmiennaLokalna = "tak zmienna lokalna to ja";
-//}
+var zmiennaGlobalna = "Tak jestem zmienną globalną i jestem widoczna w funkcjach";
+function wyswietlZmienna() {
+console.log(zmiennaGlobalna);
+    var zmiennaLokalna = "tak zmienna lokalna to ja";
+}
 //wyswietlZmienna();
 //console.log( zmiennaLokalna );
-
-var zmienna = "zmienna globalna";
-function pokazZmienna() {
-    var zmienna = "zmienna lokalna";
-    console.log( zmienna );
-}
-console.log( zmienna );
+//
+//var zmienna = "zmienna globalna";
+//function pokazZmienna() {
+//    var zmienna = "zmienna lokalna";
+//    console.log( zmienna );
+//}
+//console.log( zmienna );
