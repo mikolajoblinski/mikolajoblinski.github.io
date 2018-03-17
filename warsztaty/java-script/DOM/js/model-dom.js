992d8804edf0833198dc2de3427cd981a1b96d49
Mikołaj Obliński <mikolaj.oblinski@gmail.com>
2018-03-17 10:29:30 +0100

    model dom-podstawy

---
+++ warsztaty/java-script/DOM/js/model-dom.js
@@ -0,0 +1,13 @@
+//console.log(document);
+//console.dir(document);
+
+//zwroci element body jako obiekt
+//console.dir(document.body);
+
+//zwroci element html jako obiekt
+//console.log(document.documentElement);
+
+//obiekt window
+//console.log ( window );
+console.log ( window.innerWidth );
+console.log ( window.innerHeight );
