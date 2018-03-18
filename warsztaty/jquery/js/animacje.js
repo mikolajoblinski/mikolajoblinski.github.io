5ad2ee1f78367be0e4c043198c5850465de5be79
Mikołaj Obliński <mikolaj.oblinski@gmail.com>
2018-03-18 15:07:39 +0100

    animacje

---
+++ warsztaty/jquery/js/animacje.js
@@ -0,0 +1,15 @@
+var btn = $("#animate");
+
+btn.on("click", function(){
+//    $("h1").hide(4000).show("fast");
+//    $("h1").fadeOut(3000).fadeIn("slow");
+//    $("h1").slideUp(300).slideDown(400);
+    
+    $("h1").animate({"left" : "300px", top : "100px"},
+                    4000,
+                    function(){
+        alert("koniec animacji");
+                    }
+                    )
+    
+});
